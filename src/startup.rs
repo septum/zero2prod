3cf64e7da6ff4c33ccabbffa347c3cb8b2c9dc6f
Orlando Valverde <orlando.jesig.va@gmail.com>
2023-02-01 20:34:49 -0600

    Add subscriptions endpoint

---
+++ src/startup.rs
@@ -0,0 +1,20 @@
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+use crate::routes::{health_check, subscribe};
+
+pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
+    let connection_pool = web::Data::new(connection_pool);
+    let server = HttpServer::new(move || {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection_pool.clone())
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
