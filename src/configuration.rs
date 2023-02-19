5e634fea3c56343b3b7e707f09512abe3cedcde6
Orlando Valverde <orlando.jesig.va@gmail.com>
2023-02-19 12:38:54 -0600

    Improve validation for new subscribers

--- src/configuration.rs
+++ src/configuration.rs
@@ -5,20 +5,42 @@ use sqlx::{
     ConnectOptions,
 };
 
-#[derive(serde::Deserialize)]
+use crate::domain::SubscriberEmail;
+
+#[derive(Clone, serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
+    pub email_client: EmailClientSettings,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(Clone, serde::Deserialize)]
+pub struct EmailClientSettings {
+    pub base_url: String,
+    pub sender_email: String,
+    pub authorization_token: Secret<String>,
+    pub timeout_milliseconds: u64,
+}
+
+impl EmailClientSettings {
+    pub fn sender(&self) -> Result<SubscriberEmail, String> {
+        SubscriberEmail::parse(self.sender_email.clone())
+    }
+
+    pub fn timeout(&self) -> std::time::Duration {
+        std::time::Duration::from_millis(self.timeout_milliseconds)
+    }
+}
+
+#[derive(Clone, serde::Deserialize)]
 pub struct ApplicationSettings {
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
+    pub base_url: String,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(Clone, serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
@@ -79,6 +101,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     settings.try_deserialize::<Settings>()
 }
 
+#[derive(Clone)]
 pub enum Environment {
     Local,
     Production,
---
+++ src/routes/subscriptions_confirm.rs
@@ -0,0 +1,82 @@
+use actix_web::{web, HttpResponse};
+use sqlx::PgPool;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct Parameters {
+    subscription_token: String,
+}
+
+#[tracing::instrument(
+    name = "Confirm a pending subscriber",
+    skip(parameters, connection_pool)
+)]
+pub async fn confirm(
+    parameters: web::Query<Parameters>,
+    connection_pool: web::Data<PgPool>,
+) -> HttpResponse {
+    let id = match get_subscriber_id_from_token(&parameters.subscription_token, &connection_pool)
+        .await
+    {
+        Ok(id) => id,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+
+    match id {
+        None => HttpResponse::Unauthorized().finish(),
+        Some(subscriber_id) => {
+            if confirm_subscriber(&connection_pool, subscriber_id)
+                .await
+                .is_err()
+            {
+                return HttpResponse::InternalServerError().finish();
+            }
+            HttpResponse::Ok().finish()
+        }
+    }
+}
+
+#[tracing::instrument(
+    name = "Mark subscriber as confirmed",
+    skip(subscriber_id, connection_pool)
+)]
+pub async fn confirm_subscriber(
+    connection_pool: &PgPool,
+    subscriber_id: Uuid,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"UPDATE subscriptions SET status = 'confirmed' WHERE id = $1"#,
+        subscriber_id
+    )
+    .execute(connection_pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {e:?}");
+        e
+    })?;
+    Ok(())
+}
+
+#[tracing::instrument(
+    name = "Get subscriber_id from token",
+    skip(subscription_token, connection_pool)
+)]
+pub async fn get_subscriber_id_from_token(
+    subscription_token: &str,
+    connection_pool: &PgPool,
+) -> Result<Option<Uuid>, sqlx::Error> {
+    let result = sqlx::query!(
+        r#"
+        SELECT subscriber_id FROM subscriptions_tokens
+        WHERE subscription_token = $1
+        "#,
+        subscription_token
+    )
+    .fetch_optional(connection_pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {e:?}");
+        e
+    })?;
+    Ok(result.map(|r| r.subscriber_id))
+}
