e37d9ec32eed24735af8fd38bc3ef3264fbbd31d
Orlando Valverde <orlando.jesig.va@gmail.com>
2025-04-16 01:11:21 -0600

    Add telemetry module

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
-
-use env_logger::Env;
-use sqlx::PgPool;
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // `init` does call `set_logger`, so this is all we need to do.
-    // We are falling back to printing all logs at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    // Panic if we can't read configuration
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
-    // We have removed the hard-coded `8000` - it's now coming from our settings!
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
