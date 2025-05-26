6733a8b7e19e018d69e537e0f8171ab7f9601b63
Orlando Valverde <orlando.jesig.va@gmail.com>
2025-05-26 09:48:24 -0600

    Fix validation bug for alphanumeric chars

--- src/domain/subscription_token.rs
+++ src/domain/subscription_token.rs
@@ -17,7 +17,7 @@ impl SubscriptionToken {
 
     pub fn parse(s: String) -> Result<SubscriptionToken, String> {
         let is_empty_or_whitespace = s.trim().is_empty();
-        let is_not_alphanumeric = !s.chars().all(char::is_alphanumeric);
+        let is_not_alphanumeric = !s.chars().all(|c| c.is_ascii_alphanumeric());
         let is_not_correct_lenght = s.chars().count() != 25;
 
         if is_empty_or_whitespace || is_not_alphanumeric || is_not_correct_lenght {
