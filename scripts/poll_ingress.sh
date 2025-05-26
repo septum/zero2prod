432997685dbcd9089592018dbb2406a3c2b168c7
Orlando Valverde <orlando.jesig.va@gmail.com>
2025-05-26 11:25:07 -0600

    Fix minor nitpicks

--- scripts/poll_ingress.sh
+++ scripts/poll_ingress.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 INGRESS=$(doctl apps list --no-header --format DefaultIngress | awk '{$1=$1};1')
+
 until [[ $INGRESS != '' ]]
 do
     >&2 echo "The ingress is not available - sleeping"
     sleep 5
     INGRESS=$(doctl apps list --no-header --format DefaultIngress | awk '{$1=$1};1')
 done
+
 >&2 echo "Ingress is $INGRESS"
