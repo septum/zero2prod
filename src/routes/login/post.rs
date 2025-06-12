14b0c1b79bab9a3b7dc2287ce8463a2aeae1d41d
Orlando Valverde <orlando.jesig.va@gmail.com>
2025-06-12 12:58:23 -0600

    Process login form data

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,14 @@
-use actix_web::{HttpResponse, http::header::LOCATION};
+use actix_web::{HttpResponse, http::header::LOCATION, web};
+use secrecy::Secret;
 
-pub async fn login() -> HttpResponse {
+#[allow(unused)]
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    username: String,
+    password: Secret<String>,
+}
+
+pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::SeeOther()
         .insert_header((LOCATION, "/"))
         .finish()
