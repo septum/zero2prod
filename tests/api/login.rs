e32b2aa770fdae7e0b57a1288c6532e01cd4a690
Orlando Valverde <orlando.jesig.va@gmail.com>
2025-06-13 00:18:17 -0600

    Swap homebrew flash message for an ecosystem crate

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -11,11 +11,8 @@ async fn an_error_flash_message_is_set_on_failure() {
         "password": "random-password"
     });
     let response = app.post_login(&login_body).await;
-    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
 
     // Assert
-    assert_eq!(flash_cookie.value(), "Authentication failed");
-    assert_eq!(response.status().as_u16(), 303);
     assert_is_redirect_to(&response, "/login");
 
     // Act - Part 2
