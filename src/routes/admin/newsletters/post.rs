f6388597aa144a6c4b7e2fdad09941d02b8cf1b4
Orlando Valverde <orlando.jesig.va@gmail.com>
2025-06-16 21:25:53 -0600

    Refactor publish newsletter endpoint

--- src/routes/admin/newsletters/post.rs
+++ src/routes/admin/newsletters/post.rs
@@ -1,12 +1,10 @@
-use std::ops::Deref;
-
-use actix_web::{HttpResponse, ResponseError, http::StatusCode, web};
+use actix_web::{HttpResponse, web};
+use actix_web_flash_messages::FlashMessage;
 use anyhow::Context;
 use sqlx::PgPool;
 
 use crate::{
-    authentication::UserId, domain::SubscriberEmail, email_client::EmailClient,
-    routes::error_chain_fmt,
+    authentication::UserId, domain::SubscriberEmail, email_client::EmailClient, utils::e500,
 };
 
 #[derive(serde::Deserialize)]
@@ -21,30 +19,50 @@ pub struct Content {
     text: String,
 }
 
-struct ConfirmedSubscriber {
-    email: SubscriberEmail,
-}
-
-#[derive(thiserror::Error)]
-pub enum PublishError {
-    #[error(transparent)]
-    UnexpectedError(#[from] anyhow::Error),
-}
-
-impl std::fmt::Debug for PublishError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        error_chain_fmt(self, f)
-    }
-}
-
-impl ResponseError for PublishError {
-    fn error_response(&self) -> HttpResponse {
-        match self {
-            PublishError::UnexpectedError(_) => {
-                HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+#[tracing::instrument(
+    name = "Publish a newsletter issue",
+    skip(body, pool, email_client, user_id),
+    fields(user_id=%*user_id)
+)]
+pub async fn publish_newsletter(
+    body: web::Json<BodyData>,
+    pool: web::Data<PgPool>,
+    email_client: web::Data<EmailClient>,
+    user_id: web::ReqData<UserId>,
+) -> Result<HttpResponse, actix_web::Error> {
+    let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
+    for subscriber in subscribers {
+        match subscriber {
+            Ok(subscriber) => {
+                email_client
+                    .send_email(
+                        &subscriber.email,
+                        &body.title,
+                        &body.content.html,
+                        &body.content.text,
+                    )
+                    .await
+                    .with_context(|| {
+                        format!("Failed to send newsletter issue to {}", subscriber.email)
+                    })
+                    .map_err(e500)?;
+            }
+            Err(error) => {
+                tracing::warn!(
+                error.cause_chain = ?error,
+                error.message = %error,
+                "Skipping a confirmed subscriber. Their stored contact details are invalid",
+                );
             }
         }
     }
+
+    FlashMessage::info("The newsletter issue has been published!").send();
+    Ok(HttpResponse::Ok().finish())
+}
+
+struct ConfirmedSubscriber {
+    email: SubscriberEmail,
 }
 
 #[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
@@ -68,49 +86,4 @@ async fn get_confirmed_subscribers(
     .collect();
 
     Ok(confirmed_subscribers)
-}
-
-#[tracing::instrument(
-    name = "Publish a newsletter issue",
-    skip(body, pool, email_client, user_id),
-    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
-)]
-pub async fn publish_newsletter(
-    body: web::Json<BodyData>,
-    pool: web::Data<PgPool>,
-    email_client: web::Data<EmailClient>,
-    user_id: web::ReqData<UserId>,
-) -> Result<HttpResponse, PublishError> {
-    tracing::Span::current().record("user_id", tracing::field::display(&user_id.deref()));
-    let subscribers = get_confirmed_subscribers(&pool).await?;
-    for subscriber in subscribers {
-        match subscriber {
-            Ok(subscriber) => {
-                email_client
-                    .send_email(
-                        &subscriber.email,
-                        &body.title,
-                        &body.content.html,
-                        &body.content.text,
-                    )
-                    .await
-                    .with_context(|| {
-                        format!("Failed to send newsletter issue to {}", subscriber.email)
-                    })?;
-            }
-            Err(error) => {
-                tracing::warn!(
-                // We record the error chain as a structured field
-                // on the log record.
-                error.cause_chain = ?error,
-                // Using `\` to split a long string literal over
-                // two lines, without creating a `\n` character.
-                "Skipping a confirmed subscriber. \
-                Their stored contact details are invalid",
-                );
-            }
-        }
-    }
-
-    Ok(HttpResponse::Ok().finish())
 }
