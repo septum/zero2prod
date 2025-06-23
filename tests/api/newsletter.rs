4f441d30176d2783fc0628ef925b520c4a952d8d
Orlando Valverde <orlando.jesig.va@gmail.com>
2025-06-23 11:08:13 -0600

    Add idempotency test

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -191,3 +191,43 @@ async fn create_confirmed_subscriber(app: &TestApp) {
         .error_for_status()
         .unwrap();
 }
+
+#[tokio::test]
+async fn newsletter_creation_is_idempotent() {
+    // Arrange
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+    app.test_user.login(&app).await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // Act - Part 1 - Submit newsletter form
+    let newsletter_request_body = format!(
+        "title=Newsletter title\
+        &text_content=Newsletter body as plain text\
+        &html_content=<p>Newsletter body as HTML</p>\
+        &idempotency_key={}",
+        uuid::Uuid::new_v4().to_string()
+    );
+    let response = app.post_newsletters(&newsletter_request_body).await;
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    // Act - Part 2 - Follow the redirect
+    let html_page = app.get_newsletters_html().await;
+    assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
+
+    // Act - Part 3 - Submit newsletter form **again**
+    let response = app.post_newsletters(&newsletter_request_body).await;
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    // Act - Part 4 - Follow the redirect
+    let html_page = app.get_newsletters_html().await;
+    assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
+
+    // Mock verifies on Drop that we have sent the newsletter email **once**
+}
